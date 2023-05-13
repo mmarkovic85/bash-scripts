0a0623eebbd6e0a131d9234ab8146b4cea2a730b
Miloš Marković <milosmarkovic85@gmail.com>
2023-05-14 00:17:39 +0200

    tools: added mfx script

---
+++ tools/mfx.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Make Files eXecutable
+
+# get full path to commands
+readonly fp_echo=$(command -v echo)
+readonly fp_exit=$(command -v exit)
+readonly fp_sudo=$(command -v sudo)
+readonly fp_chmod=$(command -v chmod)
+
+# resolve directory variable
+if [ ! -d "$1" ]
+then
+	$fp_echo "ERROR: Script first argument must be valid directory" >&2
+	$fp_exit 1
+fi
+
+# make all files in directory executable
+for file in $1/*
+do
+	if [ ! -x "$file" ]
+	then
+		$fp_sudo $fp_chmod +x "$file"
+	fi
+done
